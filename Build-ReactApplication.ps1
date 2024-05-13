042aa4d400279a348256a2f7923f1a93acb4de8a
Blake14 <blakemcpherson2@gmail.com>
2024-05-13 16:35:00 -0500

    bug: file creation fix

--- Build-ReactApplication.ps1
+++ Build-ReactApplication.ps1
@@ -1,14 +1,14 @@
 # Ask for user input
 $projectName = Read-Host "Enter the project name"
 $repoVisibility = Read-Host "Private or Public repo? [private/public]"
-$templateType = Read-Host "Template type (React-Bootstrap, React-Tailwind)"
+$templateType = Read-Host "Template type (bootstrap, tailwind)"
 
 
 # Create GitHub repository
 gh repo create $projectName --$repoVisibility --confirm
 # Clone the repo locally
 git clone https://github.com/Blake14/$projectName
-
+New-Item -Path ./$projectName -type Directory
 Set-Location $projectName
 
 npx create-react-app client
@@ -18,16 +18,16 @@ Set-Location client
 Remove-Item src/logo.svg, src/App.test.js, src/reportWebVitals.js, src/setupTests.js
 
 # Replace App.js with a template file based on selected template
-if ($templateType -eq "React-Bootstrap") {
-    Copy-Item "../templates/reactjs/App.js" -Destination "src/App.js" -Force
+if ($templateType -eq "bootstrap") {
+    Copy-Item "../../templates/reactjs/App.js" -Destination "src/App.js" -Force
 }
 
 # Add necessary dependencies
 switch ($templateType) {
-    "Tailwind" {
+    "tailwind" {
         yarn add react, react-bootstrap, react-bootstrap, react-icons, tailwindcss@npm:@tailwindcss/postcss7-compat @tailwindcss/postcss7-compat postcss@^7 autoprefixer@^9
     }
-    "Bootstrap" {
+    "bootstrap" {
         yarn add react, react-bootstrap, react-bootstrap, react-icons
     }
 }
